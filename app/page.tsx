e6cac93e484feb424ec0b757660d4f3cb50d8889
YM202c2100 <Y.M202c2100@gmail.com>
2024-12-31 23:45:24 +0900

    [style] 見栄えが良くなかったので影を削除

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     <RoundResultContext value={roundResult}>
       <div className="grid grid-cols-2 h-lvh p-10 gap-3 bg-[#f4f1ce]">
         <div className="grid grid-rows-10 gap-3">
-          <div className="border-4 border-orange-300 bg-orange-50 row-span-7 rounded-md shadow-2xl relative overflow-hidden">
+          <div className="border-4 border-orange-300 bg-orange-50 row-span-7 rounded-md relative overflow-hidden">
             <p className={`absolute left-1/2 -translate-x-1/2 text-4xl mt-5 ${dotFont.className}`}>レースを進める</p>
             <RollAction roundNum={roundNum} setRoundResult={setRoundResult}/>
           </div>
