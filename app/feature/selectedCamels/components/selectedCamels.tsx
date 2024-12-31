a1ba3ddfafd1fba075a595fa66a396a9264b587d
YM202c2100 <y.m202c2100@gmail.com>
2024-12-31 16:19:24 +0900

    [feat] 当選したラクダを表示するように変更

--- app/feature/selectedCamels/components/selectedCamels.tsx
+++ app/feature/selectedCamels/components/selectedCamels.tsx
@@ -1,25 +1,32 @@
 import { CamelIcon } from "@/app/components/camelIcon"
+import { RoundResultContext } from "@/app/page"
+import { useContext } from "react"
+import { RollResult } from "../../rollAction/models/models"
 
 export const SelectedCamels:React.FC = ()=>{
+  const roundRollResult = useContext(RoundResultContext)
+  const roundResultfilledNull = [...roundRollResult, ...Array(5-roundRollResult.length).fill(null)]
   return(<>
-    <CamelCard/>
-    <CamelCard/>
-    <CamelCard/>
-    <CamelCard/>
-    <CamelCard/>
+    {roundResultfilledNull.map((result, idx) => (
+      <CamelCard result={result} idx={idx} key={idx}/>
+    ))}
   </>)
 }
 
-const CamelCard:React.FC = ()=>{
+const CamelCard:React.FC<{result:RollResult|undefined, idx:number}> = ({result, idx})=>{
   return(
-    <div className="flex justify-center items-end relative border-4 rounded-md">
-      <p className="absolute top-0 right-0 m-1">1頭目</p>
-      <div className="w-[100px] aspect-square absolute z-10 -scale-x-100 bottom-6 left-10">
-        <CamelIcon camelColor="black"/>
-      </div>
-      <div className="w-[75%] h-[30%] -skew-x-[45deg] bg-green-200 relative bottom-3 rounded-md flex justify-end items-center">
-        <p className="text-3xl mr-5">3</p>
-      </div>
+    <div className="flex justify-center items-end relative border-4 border-gray-400 rounded-md">
+      <p className="absolute top-0 right-0 m-1">{idx+1}頭目</p>
+      {result &&
+        <>
+          <div className="w-[100px] aspect-square absolute z-10 -scale-x-100 bottom-6 left-10">
+            <CamelIcon camelColor={result.camel}/>
+          </div>
+          <div className="w-[75%] h-[30%] -skew-x-[45deg] bg-green-200 relative bottom-3 rounded-md flex justify-end items-center">
+            <p className="text-3xl mr-5">{result.dice}</p>
+          </div>
+        </>
+      }
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,20 +13,20 @@ export default function Home() {
   const [roundNum, setRoundNum] = useState<number>(1)
   
   return (
-    <div className="grid grid-cols-2 h-lvh p-10 gap-3">
-      <div className="grid grid-rows-10 gap-3">
-        <div className="bg-red-300 row-span-7 rounded-md">
-          <RoundResultContext value={roundResult}>
-            <RollAction roundNum={roundNum} setRoundResult={setRoundResult}/>
-          </RoundResultContext>
+    <RoundResultContext value={roundResult}>
+      <div className="grid grid-cols-2 h-lvh p-10 gap-3">
+        <div className="grid grid-rows-10 gap-3">
+          <div className="bg-red-300 row-span-7 rounded-md">
+              <RollAction roundNum={roundNum} setRoundResult={setRoundResult}/>
+          </div>
+          <div className="bg-blue-300 row-span-3 rounded-md"></div>
         </div>
-        <div className="bg-blue-300 row-span-3 rounded-md"></div>
-      </div>
 
-      <div className="grid grid-cols-2 grid-rows-3 gap-2">
-        <SelectedCamels/>
-        <NextRoundButton setRoundNum={setRoundNum}/>
+        <div className="grid grid-cols-2 grid-rows-3 gap-2">
+          <SelectedCamels/>
+          <NextRoundButton setRoundNum={setRoundNum}/>
+        </div>
       </div>
-    </div>
+    </RoundResultContext>
   );
 }
