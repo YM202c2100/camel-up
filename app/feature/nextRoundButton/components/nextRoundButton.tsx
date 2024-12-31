bde09aeee23d64eeb46b626a74ebe96fc33c4238
YM202c2100 <y.m202c2100@gmail.com>
2024-12-31 22:49:56 +0900

    [feat] 5回抽選を行わないとNextRoundButtonが押せないように変更

--- app/feature/nextRoundButton/components/nextRoundButton.tsx
+++ app/feature/nextRoundButton/components/nextRoundButton.tsx
@@ -1,11 +1,18 @@
-import { diceResultFont } from "@/app/page"
-import { Dispatch, SetStateAction } from "react"
+import { rollLimit } from "@/app/config"
+import { diceResultFont, RoundResultContext } from "@/app/page"
+import { Dispatch, SetStateAction, useContext } from "react"
 
 export const NextRoundButton:React.FC<{setRoundNum:Dispatch<SetStateAction<number>>}> = ({setRoundNum})=>{
+  const roundResult = useContext(RoundResultContext)
+  const isDisabled = (roundResult.length != rollLimit)
   return(
     <button
       onClick={incrementRound}
-      className={`bg-red-800 p-3 rounded-3xl m-8 text-white text-6xl ${diceResultFont.className}`}
+      className={`p-3 m-8 text-6xl rounded-3xl
+        ${isDisabled ? "bg-gray-400 text-gray-300":"bg-red-800 text-white"} 
+        ${diceResultFont.className}`
+      }
+      disabled={isDisabled}
     >
       <p className="text-start">NEXT</p>
       <p className="text-end">ROUND</p>
