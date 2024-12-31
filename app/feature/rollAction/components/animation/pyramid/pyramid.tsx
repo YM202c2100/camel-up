15c897ca457ac8cdd3a621488134d2e94f157a60
YM202c2100 <y.m202c2100@gmail.com>
2024-12-31 11:31:39 +0900

    [feat] ピラミッドの画像を配置

--- app/feature/rollAction/components/animation/pyramid/pyramid.tsx
+++ app/feature/rollAction/components/animation/pyramid/pyramid.tsx
@@ -1,10 +1,11 @@
+import { PyramidImage } from "./pyramidImage"
 import { SandSmoke } from "./sandSmoke"
 
 export const Pyramid:React.FC = ()=>{
   return(
     <div className="flex items-end justify-center">
       <SandSmoke side="left"/>
-      <div className="w-[300px] h-[300px] bg-red-300 animate-pyramid"></div>
+      <PyramidImage/>
       <SandSmoke side="right"/>
     </div>
   )
