701f6dfe51d7d371670f90505221babec3654308
YM202c2100 <y.m202c2100@gmail.com>
2025-01-03 11:21:39 +0900

    [chore] ラウンド収益の計算時以外でもウィンドウが表示されるため説明文を修正

--- app/feature/player/components/score.tsx
+++ app/feature/player/components/score.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export const Score:React.FC<PlayerCardProps> = ({info, setInfo})=>{
   const [isAcceptingEntries, setIsAccepted] = useState<boolean>(false)
-  const [roundReturns, setReturns] = useState<number>(1)
+  const [returns, setReturns] = useState<number>(1)
   return(
     <div className="relative">
       <p 
@@ -17,13 +17,13 @@ export const Score:React.FC<PlayerCardProps> = ({info, setInfo})=>{
       </p>
 
       {isAcceptingEntries &&
-        <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-[110%] z-10 w-[250px] bg-white p-3 rounded-md shadow-2xl border-4 border-gray-800">
-          <p className={`text-2xl ${dotFont.className}`}>ラウンド収益を入力</p>
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-[110%] z-10 w-[200px] bg-white p-3 rounded-md shadow-2xl border-4 border-gray-800">
+          <p className={`text-2xl text-center ${dotFont.className}`}>収益を入力</p>
           <input type="number" 
             onChange={e => setReturns(parseInt(e.target.value))}
             onKeyDown={e => {
               if(e.key === "Enter"){
-                setInfo({type:"addScore", value:roundReturns})
+                setInfo({type:"addScore", value:returns})
                 setIsAccepted(false)
               }
             }}
