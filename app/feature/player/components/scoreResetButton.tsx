a1c8bfbc5480303265e75ef6fa762303ec6c6cbe
YM202c2100 <y.m202c2100@gmail.com>
2025-01-10 23:08:49 +0900

    [feat] ボタンホバー時にツールチップが表示されるように変更

--- app/feature/player/components/scoreResetButton.tsx
+++ app/feature/player/components/scoreResetButton.tsx
@@ -1,19 +1,39 @@
 import { SetInfoAction } from "@/app/models/playerCards.model"
 import { dotFont } from "@/app/page"
+import { useState } from "react"
 
 type ResetButtonProps = {
   setPlayersInfo: (action: SetInfoAction&{idx:number})=>void
 }
 export const ScoreResetButton:React.FC<ResetButtonProps> = ({setPlayersInfo})=>{
+  const [isHover, setIsHover] = useState<boolean>(false)
   return(
     <div
-      className={`w-[30px] aspect-square bg-white rounded-full
-        border-2 border-gray-300
-        fixed top-2 left-2 cursor-pointer
-        text-center ${dotFont.className} text-gray-400`}
-      onClick={()=>setPlayersInfo({type:"resetScore", idx:0})}
+    onClick={()=>setPlayersInfo({type:"resetScore", idx:0})}
+    onMouseEnter={()=>setIsHover(true)}
+    onMouseLeave={()=>setIsHover(false)}
+    className="w-[30px] aspect-square bg-white rounded-full
+      border-2 border-gray-300
+      fixed top-2 left-2 
+      cursor-pointer"
     >
-      0
+      <div className={`text-center ${dotFont.className} text-gray-400`}>0</div>
+      <Tooltip isHover={isHover}/>
+    </div>
+  )
+  
+}
+
+const Tooltip:React.FC<{isHover:boolean}> = ({isHover})=>{
+  return(
+    <div 
+      className={`absolute top-[-2px] right-[-5px] translate-x-[100%] 
+        whitespace-nowrap bg-white p-1 border border-black
+        text-sm text-black
+        ${isHover ? "opacity-100":"opacity-0"} transition-opacity duration-200
+        pointer-events-none`}
+    >
+      スコアをリセット
     </div>
   )
 }
