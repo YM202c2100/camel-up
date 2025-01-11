522bc451a493f146c1bc5d49d4ad806d1cc4428a
YM202c2100 <y.m202c2100@gmail.com>
2025-01-11 21:19:10 +0900

    [fix] 想定していないデバイスでページを表示した際に、背景色が指定されていない領域があったため修正

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
   return (
     <RoundResultContext value={roundResult}>
-      <div className="grid grid-cols-20 h-lvh p-10 gap-3 bg-[#f4f1ce]">
+      <div className="grid grid-cols-20 h-lvh p-10 gap-3">
         <PlayerCards/>
         <div className="col-span-10 grid grid-rows-10 gap-3">
           <div className="border-4 border-orange-300 bg-orange-50 row-span-7 rounded-md shadow-2xl relative overflow-hidden">
